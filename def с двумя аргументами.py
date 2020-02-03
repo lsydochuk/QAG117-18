287692e2faa75dd9da2f2aca3f1943dd532e057a
Larysa Sydorchuk <larysa.sydorchuk@gmail.com>
2020-02-03 15:19:33 +0200

    Dz3_Def_v.1.0

---
+++ def с двумя аргументами.py
@@ -0,0 +1,8 @@
+# функция, которая создает список исходя из двух аргументов
+a = int(input("Укажите длину списка "))
+b = int(input("Укажите максимально возможное значение элемента списка "))
+import random
+def my_function():
+    print(list(x))
+x = (random.choices(range(b), k=a))
+my_function()
